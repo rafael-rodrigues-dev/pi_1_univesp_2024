f30ae44118d01b68c1d5ecd2fbdb01e0098ecea3
Victor Salustrino Bezerra <salustrino@gmail.com>
2024-03-22 17:16:45 -0300

    Criação do modelo Projeto, com acesso atravé do Admin

--- esp_tec/app_gerenciador/models.py
+++ esp_tec/app_gerenciador/models.py
@@ -1,3 +1,15 @@
+"""
+    Definição dos modelos.
+"""
 from django.db import models
 
 # Create your models here.
+
+
+class Projeto(models.Model):
+    """
+    Modelo da persistência de Projetos
+    """
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField()
+    dt_criacao = models.DateField()
