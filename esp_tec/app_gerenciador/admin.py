53bd45e2013965cb578708aa1c6df8f4caa54b61
Victor Salustrino Bezerra <salustrino@gmail.com>
2024-03-25 21:08:52 -0300

    Lista os projetos e materiais em ordem alfabética.

--- esp_tec/app_gerenciador/admin.py
+++ esp_tec/app_gerenciador/admin.py
@@ -9,8 +9,14 @@ class ProjetoAdmin(admin.ModelAdmin):
     """ Classe admin para o Projeto """
     list_display = ("nome", "dt_criacao")
 
+    def get_ordering(self, request):
+        return ['nome', ]
+
 
 @admin.register(Material)
 class MaterialAdmin(admin.ModelAdmin):
     """ Classe admin para o Material """
     list_display = ("nome", "unidade_medida")
+
+    def get_ordering(self, request):
+        return ['nome', ]
