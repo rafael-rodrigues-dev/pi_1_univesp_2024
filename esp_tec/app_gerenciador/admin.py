79bb299977c536d5a6ed3707d1688b00b37a4d70
Victor Salustrino Bezerra <salustrino@gmail.com>
2024-03-25 22:55:36 -0300

    link do pdf da listagem de projetos

--- esp_tec/app_gerenciador/admin.py
+++ esp_tec/app_gerenciador/admin.py
@@ -2,15 +2,26 @@ from django.contrib import admin
 
 # Register your models here.
 
+from django.utils.html import format_html
+from django.urls import reverse_lazy
+
 from .models import Projeto, Material
 
 @admin.register(Projeto)
 class ProjetoAdmin(admin.ModelAdmin):
     """ Classe admin para o Projeto """
-    list_display = ("nome", "dt_criacao")
+    list_display = ("nome", "dt_criacao", "pdf_url")
 
     def get_ordering(self, request):
         return ['nome', ]
+    
+    def pdf_url(self, obj):
+        """ Cria a link do pdf para ser exibido na coluna da listagem """
+        url =  reverse_lazy('report_pdf', kwargs={'pk': obj.id})
+        return format_html(f"<a target='_blank' href='{url}'>PDF</a>")
+    
+    pdf_url.allow_tags = True
+    pdf_url.short_description = 'Especificação Técnica'
 
 
 @admin.register(Material)
